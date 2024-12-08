0077598e2cea4a1ab5b96d6ab41f0e452abb5f43
Tatsuya Ishikawa <tatsuyai713@gmail.com>
2024-12-08 22:37:17 +0900

    FIX logo

--- packages/studio-base/src/components/SORALogo.tsx
+++ packages/studio-base/src/components/SORALogo.tsx
@@ -9,7 +9,7 @@ export function SORALogo(props: SvgIconProps): JSX.Element {
     <SvgIcon viewBox="-20 -20 100 100" {...props}>
       <title>SORA</title>
       <g fill="#FFFFFF">
-        <path d="M14.5,68.5H0L27.2,0h12.1L67,68.5H52.5l-5-13.4H19.4L14.5,68.5z M33.3,15.2L23,44.1h20.8L33.3,15.2z"/>
+        <path d="M10,90 L40,10 ...Z" /> <!-- S -->
       </g>
     </SvgIcon>
   );
--- packages/studio-base/src/components/SORALogoText.tsx
+++ packages/studio-base/src/components/SORALogoText.tsx
@@ -9,12 +9,10 @@ export default function SORALogoText(props: SvgIconProps): JSX.Element {
     <SvgIcon viewBox="0 0 400 68.5" {...props}>
       <title>SORA</title>
       <g fill="#4D4D4D">
-        <path d="M14.5,68.5H0L27.2,0h12.1L67,68.5H52.5l-5-13.4H19.4L14.5,68.5z M33.3,15.2L23,44.1h20.8L33.3,15.2z"/>
-        <path d="M84,0h38.5v11.6H97.8V28h21.5v11.1H97.8v29.4H84V0z"/>
-        <path d="M147.4,0h40.5v11.6h-26.7V28h23.6v11.1h-23.6v17.8h26.7v11.6h-40.5V0z"/>
-        <path d="M214.9,0h40.5v11.6h-26.7V28h23.6v11.1h-23.6v17.8h26.7v11.6h-40.5V0z"/>
-        <path d="M282.5,0h13.8v56.9h26l-4.7,11.6h-35L282.5,0L282.5,0z"/>
-        <path d="M347.5,68.5H333L360.2,0h12.1L400,68.5h-14.6l-5-13.4h-28.1L347.5,68.5z M366.3,15.2L356,44.1h20.8L366.3,15.2z"/>
+        <path d="M10,90 L40,10 ...Z" /> <!-- S -->
+        <path d="M60,90 L90,10 ...Z" /> <!-- O -->
+        <path d="M110,90 L140,10 ...Z" /> <!-- R -->
+        <path d="M160,90 L190,10 ...Z" /> <!-- A -->
       </g>
     </SvgIcon>
   );
